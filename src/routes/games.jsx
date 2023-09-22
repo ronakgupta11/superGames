a314f46df8b5e1d7d0ae268e0878077d6c2f7f7a
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-09-23 00:04:31 +0530

    game image update

--- src/routes/games.jsx
+++ src/routes/games.jsx
@@ -5,9 +5,9 @@ import GamesCard from '../components/GamesCard'
 function Games() {
   return (
     <div className='p-4 flex flex-wrap space-y-2 flex-1 w-4/5 mx-auto my-16 space-x-2 items-center justify-between'>
-<GamesCard img = "https://picsum.photos/150" link="/games/bankpanic" Title="bank Panic" desc="random bank panic game to shoot the invadors"/>
-<GamesCard img = "https://picsum.photos/150" link="/games/cardmemory" Title="bank Panic" desc="random bank panic game to shoot the invadors"/>
-<GamesCard img = "https://picsum.photos/150" Title="bank Panic" desc="random bank panic game to shoot the invadors"/>
+<GamesCard img = "../assets/logo/bp.jpeg" link="/games/bankpanic" Title="Bank Panic" desc="random bank panic game to shoot the invadors"/>
+<GamesCard img = "../assets/logo/cm.jpeg" link="/games/cardmemory" Title="Card Memory Game" desc="check your memory with cards flipping game"/>
+{/* <GamesCard img = "https://picsum.photos/150" Title="bank Panic" desc="random bank panic game to shoot the invadors"/> */}
 
 
     </div>
