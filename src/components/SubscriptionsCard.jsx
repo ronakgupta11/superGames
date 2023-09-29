770debe5e7851b66402adb1945af4b61032e131d
Ronak Gupta <mailtoronakgupta@gmail.com>
2023-09-29 12:08:11 +0530

    added subscription Info

--- src/components/SubscriptionsCard.jsx
+++ src/components/SubscriptionsCard.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Card } from 'flowbite-react';
+import { Button, Card } from 'flowbite-react';
 import {BsCheckCircleFill} from "react-icons/bs"
 import SubscriptionPage from './SubscriptionPage';
 export default function SubcsriptionsCard(props) {
@@ -14,63 +14,64 @@ export default function SubcsriptionsCard(props) {
         ETHx
       </span>
       <span className="text-3xl font-extrabold tracking-tight">
-        { props.price}
+        {props.price}
       </span>
       <span className="ml-1 text-xl font-normal text-gray-500 dark:text-gray-400">
         /month
       </span>
     </div>
     <ul className="my-7 space-y-5">
-      <li className="flex space-x-3">
+      <li className={props.item1?"flex space-x-3":"flex space-x-3 line-through decoration-gray-500"}>
         <BsCheckCircleFill/>
         <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-          {props.item1}
+        Access To Free Movies
         </span>
       </li>
-      <li className="flex space-x-3">
-      <BsCheckCircleFill/>
-
-        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-          {props.item2}
-        </span>
-      </li>
-      <li className="flex space-x-3">
-      <BsCheckCircleFill/>
-
-        <span className="text-base font-normal leading-tight text-gray-500 dark:text-gray-400">
-          {props.item3}
-        </span>
-      </li>
-      <li className="flex space-x-3 line-through decoration-gray-500">
+      <li className= {props.item2?"flex space-x-3":"flex space-x-3 line-through decoration-gray-500"}>
       <BsCheckCircleFill/>
 
         <span className="text-base font-normal leading-tight text-gray-500">
-          {props.item4}
+        Access to Basic Movies
         </span>
       </li>
-      <li className="flex space-x-3 line-through decoration-gray-500">
+      <li className = {props.item3?"flex space-x-3":"flex space-x-3 line-through decoration-gray-500"}>
+      <BsCheckCircleFill/>
+ 
+        <span className="text-base font-normal leading-tight text-gray-500">
+        Access to Game Play
+        </span>
+      </li>
+      <li className= {props.item4?"flex space-x-3":"flex space-x-3 line-through decoration-gray-500"}>
       <BsCheckCircleFill/>
 
         <span className="text-base font-normal leading-tight text-gray-500">
-          {props.item5}
+        Access to Premium Movies
         </span>
       </li>
-      <li className="flex space-x-3 line-through decoration-gray-500">
+      <li className={props.item5?"flex space-x-3":"flex space-x-3 line-through decoration-gray-500"}>
       <BsCheckCircleFill/>
 
         <span className="text-base font-normal leading-tight text-gray-500">
-          {props.item6}
+        Access To Streams
         </span>
       </li>
-      <li className="flex space-x-3 line-through decoration-gray-500">
+      <li className={props.item6?"flex space-x-3":"flex space-x-3 line-through decoration-gray-500"}>
       <BsCheckCircleFill/>
 
         <span className="text-base font-normal leading-tight text-gray-500">
-          {props.item7}
+        Add Your Content To Platfrom
+        </span>
+      </li>
+      <li className={props.item7?"flex space-x-3":"flex space-x-3 line-through decoration-gray-500"}>
+      <BsCheckCircleFill/>
+
+        <span className="text-base font-normal leading-tight text-gray-500">
+        Create And Broad Live Streams
         </span>
       </li>
     </ul>
-    <SubscriptionPage price = {props.price}/>
+    {props.price != "0" && <SubscriptionPage price = {props.price}/>}
+    {props.price == "0"&& <Button className='bg-blue-700 enabled:hover:bg-blue-800 focus:ring-blue-300'>FREE</Button>}
   </Card>
     
   )
--- src/routes/pricing.jsx
+++ src/routes/pricing.jsx
@@ -5,9 +5,9 @@ import SubscriptionsCard from '../components/SubscriptionsCard'
 function Pricing() {
   return (
     <div className='p-4 flex flex-wrap flex-1 w-4/5 mx-auto my-16 space-x-4 items-center justify-between'>
-        <SubscriptionsCard plan = "Basic" price="0.001"/>
-        <SubscriptionsCard plan="Intermediate" price = "0.002"/>
-        <SubscriptionsCard plan="Advanced" price = "0.003"/>
+        <SubscriptionsCard plan = "FREE" price="0" item1={true}/>
+        <SubscriptionsCard plan="BASIC"  price = "0.001" item1={true} item2 = {true} item3 ={true} />
+        <SubscriptionsCard plan="PREMIUM" item1={true} item2 = {true} item3 ={true} item4 ={true} item5={true} item6={true} item7={true} price = "0.002"/>
 
     </div>
   )
